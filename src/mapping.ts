db447151bb134e152b80e0e13578c0aadff06db7
Frank <frank.pfeift@protonmail.com>
2021-09-04 13:31:00 +0200

    feat: added transfer entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,71 +6,23 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { TokenId } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleTransfer(event: Transfer): void {
+  let tokenEntity = TokenId.load(event.params.tokenId.toString());
+  if(tokenEntity === null) {
+    // create all
+    createAllEntities();
+    tokenEntity = TokenId.load(event.params.tokenId.toString());
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getChest(...)
-  // - contract.getFoot(...)
-  // - contract.getHand(...)
-  // - contract.getHead(...)
-  // - contract.getNeck(...)
-  // - contract.getRing(...)
-  // - contract.getWaist(...)
-  // - contract.getWeapon(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+  tokenEntity.owner = event.params.to;
+  tokenEntity.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
+function createAllEntities(): void {
+  for(let i=1; i<7778; i++) {
+    let tokenEntity = new TokenId(i.toString());
+    tokenEntity.save();
+  }
+}
